b1a7b43222d8498c1091b3a5735e0ef2d5ee7c58
adib424 <44995399+adib424@users.noreply.github.com>
2024-09-05 18:58:40 +0700

    Proxy HK

--- _worker.js
+++ _worker.js
@@ -5,7 +5,7 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = '989a82a9-d4b5-4786-b15f-cbac66616f43';
 
-const พร็อกซีไอพีs = ['141.147.170.100'];
+const พร็อกซีไอพีs = ['8.210.92.27'];
 
 // if you want to use ipv6 or single พร็อกซีไอพี, please add comment at this line and remove comment at the next line
 let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
